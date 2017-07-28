e4029b197e9bb0efe73760f347c74620c83a88f1
pnvaidya <priya.n.vaidya@intel.com>
2017-07-28 14:52:40 -0400

    smithwaterman updates

---
+++ src/main/native/smithwaterman/CMakeFiles/gkl_smithwaterman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gkl_smithwaterman.dir/IntelSmithWaterman.o"
+  "libgkl_smithwaterman.pdb"
+  "libgkl_smithwaterman.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gkl_smithwaterman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
